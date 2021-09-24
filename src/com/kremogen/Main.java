d9e0b4c7f8d821935c11f65512b8a03b7d9caf91
Kalganov Nikita <nik.kosachev@mail.ru>
2021-09-24 19:36:10 +0300

    Main file load

--- src/com/kremogen/Main.java
+++ src/com/kremogen/Main.java
@@ -1,8 +1,27 @@
 package com.kremogen;
 
+import java.util.ArrayList;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        ArrayList<String> states = new ArrayList<String>();
+        states.add("Франция");
+        states.add("Великобритания");
+        states.add("Испания");
+        states.add(1, "Италия");
+        System.out.println(states.get(1));
+        states.set(1, "Дания");
+        System.out.printf("В списке %d элемента(ов) \n", states.size());
+        for (String state : states) {
+            System.out.println(state);
+        }
+        if (states.contains("Германия")) {
+            System.out.println("Список содержит государство Германия");
+            Object[] countries = states.toArray();
+            for (Object country : countries) {
+                System.out.println(country);
+            }
+        }
     }
 }
